5d3373456bcefd3087c390d79f0b165de203e6c6
wuxianucw <x-wuxian@qq.com>
2021-05-10 07:52:20 +0800

    optimize code

--- public/hydropdf.page.js
+++ public/hydropdf.page.js
@@ -4,15 +4,13 @@
 
     const page = new AutoloadPage('hydropdf', async () => {
         $('div[data-hpdf]').get().forEach(async (element) => {
-            const id = `pdf-${Math.random().toString()}`;
-            $(element).attr('id', id);
             const url = new URL($(element).text(), window.location.href);
             const iframe = document.createElement('iframe');
             iframe.src = `/hydro-pdf/${encodeURIComponent(encodeURIComponent(url))}`;
             iframe.width = '100%';
             iframe.style.minHeight = '90vh';
             iframe.style.border = 'none';
-            $(element).html('').append(iframe);
+            $(element).empty().append(iframe);
         });
     });
 
